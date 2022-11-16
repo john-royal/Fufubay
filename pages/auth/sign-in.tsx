92debbb4dfb1bc0e45316aa62edd660401d90d8b
zikrya <77081809+zikrya@users.noreply.github.com>
2022-11-16 10:14:38 -0500

    Html added to sign-in.tsx [styling to be added]

--- pages/auth/sign-in.tsx
+++ pages/auth/sign-in.tsx
@@ -2,11 +2,29 @@ import Link from 'next/link'
 
 export default function SignIn () {
   return (
-    <main>
-      <h1>Sign In to Fufubay</h1>
-      <p>
-        <Link href='/'>Home</Link>, <Link href='/auth/create-account'>Create an Account</Link>
-      </p>
-    </main>
+    <div className="container forms">
+    <div className="form login">
+        <header className="head">Login</header>
+
+        <form action="#">
+            <div className="field input-field">
+                <input type="email" placeholder="Email" className="input" />
+            </div>
+            <div className="field input-field">
+                <input type="password" placeholder="Password" className="password" />
+            </div>
+            <div className="form link">
+                <a href="#" className="forgot-pass">Forgot password?</a>
+            </div>
+            <div className="field input-field">
+                <button>Login</button>
+            </div>
+            <div className="form link">
+            <Link href='/'>Home</Link>, <Link href='/auth/create-account'>Create an Account</Link>
+            </div>
+        </form>
+    </div>
+
+</div>
   )
 }
