8a917afe0237f4b778811d8e69a76154ae2d009e
zikrya <77081809+zikrya@users.noreply.github.com>
2022-11-21 01:20:49 -0500

    Basic Styling ADDED for Settings page

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -4,40 +4,52 @@ const Settings = () => {
     return (
         <div>
             <div>
-                <h1>Settings</h1>
+                <h1 className="has-text-centered is-size-1">Settings</h1>
             </div>
-            <div>--------------</div>
-            <div>
-                <h2>Profile</h2>
-                <Link href='#'>Edit</Link>
-            </div>
-            <div>---------------</div>
-            <div>
-                <h2>Account Information</h2>
-                <div>
-                    <h3>Email</h3>
-                    <Link href='#'>Edit</Link>
-                </div>
-                <div>
-                    <h3>Username</h3>
-                    <Link href='#'>Edit</Link>
-                </div>
-                <div>
-                    <h3>Password</h3>
-                    <Link href='#'>Edit</Link>
+            <div className="container">
+            <div className="box">
+                <h2 className="has-text-centered is-size-3 py-3 my-3">Profile</h2>
+                <div className="has-text-centered">
+                <Link href='#' className="button is-small">Edit</Link>
                 </div>
             </div>
-            <div>-------------</div>
-            <div>
-                <h2>Address</h2>
-                <h3>[DIPLAY ADDRESS HERE]</h3>
-                <Link href='#'>Edit</Link>
+            <div className="box" >
+                <h2 className="has-text-centered is-size-3">Account Information</h2>
+                <div className="container">
+                <div className="box">
+                    <h3 className="has-text-centered is-size-4 py-3 my-3">Email</h3>
+                    <div className="has-text-centered">
+                    <Link href='#' className="button is-small">Edit</Link>
+                    </div>
+                </div>
+                <div className="box">
+                    <h3 className="has-text-centered is-size-4 py-3 my-3">Username</h3>
+                    <div className="has-text-centered">
+                    <Link href='#' className="button is-small">Edit</Link>
+                    </div>
+                </div>
+                <div className="box">
+                    <h3 className="has-text-centered is-size-4 py-3 my-3">Password</h3>
+                    <div className="has-text-centered">
+                    <Link href='#' className="button is-small">Edit</Link>
+                    </div>
+                </div>
+                </div>
             </div>
-            <div>-------------</div>
-            <div>
-                <h2>Billing</h2>
-                <Link href='#'>Edit</Link>
+            <div className="box">
+                <h2 className="has-text-centered is-size-3 py-3 my-3">Address</h2>
+                <h3 className="has-text-centered">[DIPLAY ADDRESS HERE]</h3>
+                <div className="has-text-centered">
+                <Link href='#' className="button is-small">Edit</Link>
+                </div>
             </div>
+            <div className="box">
+                <h2 className="has-text-centered is-size-3 py-3 my-3">Billing</h2>
+                <div className="has-text-centered">
+                <Link href='#' className="button is-small">Edit</Link>
+                </div>
+            </div>
+        </div>
         </div>
      );
 }
