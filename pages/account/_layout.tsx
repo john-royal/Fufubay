9a881c35ac5a74acb759519814e5aab6f1272e59
zikrya <77081809+zikrya@users.noreply.github.com>
2022-12-09 03:52:27 -0500

    Added Nav for Past Auction [path has to be set]

--- pages/account/_layout.tsx
+++ pages/account/_layout.tsx
@@ -4,7 +4,8 @@ import { PropsWithChildren } from 'react'
 export default function AccountLayout ({ children }: PropsWithChildren) {
   const items: MenuItem[] = [
     { title: 'Settings', path: '/account/settings' },
-    { title: 'My Auctions', path: '/account/auctions' }
+    { title: 'My Auctions', path: '/account/auctions' },
+    {title: "Previous Auctions", path: "/account/auctions"}
   ]
 
   return (
