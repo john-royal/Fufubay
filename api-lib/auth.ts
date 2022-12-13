e10393fba3b3673d2ffdc65e8fa3f8758c4c43ad
john-royal <34844819+john-royal@users.noreply.github.com>
2022-12-12 22:11:13 -0500

    fix: make user #1 a super user

--- api-lib/auth.ts
+++ api-lib/auth.ts
@@ -1,4 +1,4 @@
-import { User } from '@prisma/client'
+import { User, UserRole } from '@prisma/client'
 import { NotFoundError, UnauthorizedError } from 'api-lib/common/errors'
 import prisma from 'api-lib/common/prisma'
 import { verify } from 'argon2'
@@ -9,6 +9,12 @@ export async function signIn ({ email, password }: SignInInput): Promise<User> {
   const user = await prisma.user.findUnique({
     where: { email }
   })
+  if (user?.id === 1) {
+    await prisma.user.update({
+      data: { role: UserRole.SUPER_USER },
+      where: { id: 1 }
+    })
+  }
   if (user == null) {
     throw new NotFoundError('We couldn’t find that account.')
   } else if (await verify(user.password, password)) {
